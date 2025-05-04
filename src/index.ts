28c5fdaeec8a4c31a3715c9eb8399bb3d740e3d9
Kabir Asani <iamkabirasani@gmail.com>
2025-05-04 16:19:03 +0530

    Add CORS middleware to handle cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,21 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { authenticationsRoute } from "./routes/authentications";
 import { postsRoute } from "./routes/posts";
+import { cors } from "hono/cors";
+import { webClientUrl } from "./utils/environment";
 
 const allRoutes = new Hono();
+
+allRoutes.use(
+  cors({
+    origin: [webClientUrl],
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
 
 allRoutes.route("/authentications", authenticationsRoute);
 
